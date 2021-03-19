6db12ede06035e210dfb9e0f5d13e7bc2362f3f3
Vincent Phung <vincentphung43@yahoo.com>
2021-03-18 21:51:17 -0400

    fixed queen valid path

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -36,7 +36,7 @@ public class Queen extends Piece {
 
     @Override
     public boolean pathBlocked(Square[][] board, Square cur, Square dest) {
-        int distance = Math.abs(dest.getRow() - cur.getRow());
+        int diagonalPathLength = Math.abs(dest.getRow() - cur.getRow());
         int rowDistance = dest.getRow() - cur.getRow();
         int colDistance = dest.getCol() - cur.getCol();
         int row = cur.getRow();
@@ -44,7 +44,7 @@ public class Queen extends Piece {
         
         if(Math.abs(rowDistance) == Math.abs(colDistance)) {
             //check path diagonal BETWEEN cur and dest(excluding)
-            for(int i = 0; i < distance-1; i++) {
+            for(int i = 0; i < diagonalPathLength-1; i++) {
                 //If dest square is below, decrement distance by 1 each interval
                 //if dest square is above, increment distance by 1 each interval
                 int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
@@ -57,7 +57,8 @@ public class Queen extends Piece {
             }
         }
         else if(rowDistance == 0) {
-            for(int i = 0; i < distance-1; i++) {
+            int pathLength = colDistance;
+            for(int i = 0; i < pathLength-1; i++) {
                 Piece curPiece;
                 
                 int colDirection = (colDistance > 0) ? 1+i : -1-i;
@@ -69,7 +70,8 @@ public class Queen extends Piece {
             }
         }
         else {
-            for(int i = 0; i < distance-1; i++) {
+            int pathLength = rowDistance;
+            for(int i = 0; i < pathLength-1; i++) {
                 Piece curPiece;
                 
                 int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
