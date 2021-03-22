45b08a77c33899f991f86d57e14c1e03d67c18b2
Vincent Phung <vincentphung43@yahoo.com>
2021-03-22 13:32:37 -0400

    fix queen path

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -14,18 +14,17 @@ public class Queen extends Piece {
 
     @Override
     public boolean validMove(Board boardObject, Square cur, Square dest) {
-    	
-    	Square[][] board = boardObject.board;
-    	
+
+        Square[][] board = boardObject.board;
+
         int rowDistance = Math.abs(cur.getRow() - dest.getRow());
         int colDistance = Math.abs(cur.getCol() - dest.getCol());
 
         if ((rowDistance != 0 && colDistance != 0) && (rowDistance != colDistance)) {
             return false;
-        }
-        else {
-        	//if path isn't blocked move is valid
-        	return !pathBlocked(board, cur, dest);
+        } else {
+            // if path isn't blocked move is valid
+            return !pathBlocked(board, cur, dest);
         }
 
     }
@@ -41,50 +40,47 @@ public class Queen extends Piece {
         int colDistance = dest.getCol() - cur.getCol();
         int row = cur.getRow();
         int col = cur.getCol();
-        
-        if(Math.abs(rowDistance) == Math.abs(colDistance)) {
-            //check path diagonal BETWEEN cur and dest(excluding)
-            for(int i = 0; i < diagonalPathLength-1; i++) {
-                //If dest square is below, decrement distance by 1 each interval
-                //if dest square is above, increment distance by 1 each interval
-                int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
-                int colDirection = (colDistance > 0) ? 1+i : -1-i;
-                
-                Piece curPiece = board[row+rowDirection][col+colDirection].getPiece();
-                if(curPiece != null) {
+
+        if (Math.abs(rowDistance) == Math.abs(colDistance)) {
+            // check path diagonal BETWEEN cur and dest(excluding)
+            for (int i = 0; i < diagonalPathLength - 1; i++) {
+                // If dest square is below, decrement distance by 1 each interval
+                // if dest square is above, increment distance by 1 each interval
+                int rowDirection = (rowDistance > 0) ? 1 + i : -1 - i;
+                int colDirection = (colDistance > 0) ? 1 + i : -1 - i;
+
+                Piece curPiece = board[row + rowDirection][col + colDirection].getPiece();
+                if (curPiece != null) {
                     return true;
                 }
             }
-        }
-        else if(rowDistance == 0) {
+        } else if (rowDistance == 0) {
             int pathLength = Math.abs(colDistance);
-            for(int i = 0; i < pathLength-1; i++) {
+            for (int i = 0; i < pathLength - 1; i++) {
                 Piece curPiece;
-                
-                int colDirection = (colDistance > 0) ? 1+i : -1-i;
-                curPiece = board[row][col+colDirection].getPiece();
-                
-                if(curPiece != null) {
+
+                int colDirection = (colDistance > 0) ? 1 + i : -1 - i;
+                curPiece = board[row][col + colDirection].getPiece();
+
+                if (curPiece != null) {
                     return true;
                 }
             }
-        }
-        else {
+        } else {
             int pathLength = Math.abs(rowDistance);
-            for(int i = 0; i < pathLength-1; i++) {
+            for (int i = 0; i < pathLength - 1; i++) {
                 Piece curPiece;
-                
-                int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
+
+                int rowDirection = (rowDistance > 0) ? 1 + i : -1 - i;
                 curPiece = board[row + rowDirection][col].getPiece();
-                
-                
-                if(curPiece != null) {
+
+                if (curPiece != null) {
                     return true;
                 }
             }
         }
 
-    	return false;
+        return false;
     }
 
     @Override
@@ -96,26 +92,26 @@ public class Queen extends Piece {
         int col = cur.getCol();
         ArrayList<Square> path = new ArrayList<>();
 
-        if(Math.abs(rowDistance) == Math.abs(colDistance)) {
-            //check path diagonal BETWEEN cur and dest(excluding)
-            for(int i = 0; i < pathLength-1; i++) {
-                //If dest square is below, decrement distance by 1 each interval
-                //if dest square is above, increment distance by 1 each interval
-                int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
-                int colDirection = (colDistance > 0) ? 1+i : -1-i;
-                
-                path.add(board[row+rowDirection][col+colDirection]);
+        if (Math.abs(rowDistance) == Math.abs(colDistance)) {
+            // check path diagonal BETWEEN cur and dest(excluding)
+            for (int i = 0; i < pathLength - 1; i++) {
+                // If dest square is below, decrement distance by 1 each interval
+                // if dest square is above, increment distance by 1 each interval
+                int rowDirection = (rowDistance > 0) ? 1 + i : -1 - i;
+                int colDirection = (colDistance > 0) ? 1 + i : -1 - i;
+
+                path.add(board[row + rowDirection][col + colDirection]);
             }
-        }
-        else if(rowDistance == 0) {
-            for(int i = 0; i < pathLength-1; i++) {
-                int colDirection = (colDistance > 0) ? 1+i : -1-i;
-                path.add(board[row][col+colDirection]);
+        } else if (rowDistance == 0) {
+            pathLength = Math.abs(colDistance);
+            for (int i = 0; i < pathLength - 1; i++) {
+                int colDirection = (colDistance > 0) ? 1 + i : -1 - i;
+                path.add(board[row][col + colDirection]);
             }
-        }
-        else {
-            for(int i = 0; i < pathLength-1; i++) {                
-                int rowDirection = (rowDistance > 0) ? 1+i : -1-i;
+        } else {
+            pathLength = Math.abs(rowDistance);
+            for (int i = 0; i < pathLength - 1; i++) {
+                int rowDirection = (rowDistance > 0) ? 1 + i : -1 - i;
                 path.add(board[row + rowDirection][col]);
             }
         }
