511a050c2748d79dbde10bb78a96d04b9166ebb6
Vincent Phung <vincentphung43@yahoo.com>
2021-03-23 14:07:38 -0400

    fixed javadocs

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"chess","l":"Board"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pieces","l":"Bishop"},{"p":"chess","l":"Board"},{"p":"chess","l":"Chess"},{"p":"chess","l":"Game"},{"p":"pieces","l":"King"},{"p":"pieces","l":"Knight"},{"p":"pieces","l":"Pawn"},{"p":"pieces","l":"Piece"},{"p":"pieces","l":"Queen"},{"p":"pieces","l":"Rook"},{"p":"chess","l":"Square"}]
